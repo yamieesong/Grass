37fc2269cd7c8dada285806d1e933c1585343e49
yamieesong <yamiee@nate.com>
2022-07-13 18:04:59 +0900

    javaStudy_220713

---
+++ src/com/grassjavastudy/ch01/FirstJavaClass.java
@@ -0,0 +1,20 @@
+package com.grassjavastudy.ch01;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		int x = 30;
+		int y = 50;
+		
+		char ch = 'A';
+		
+		String hello = "Hello Java";
+		
+		System.out.println(ch);
+		System.out.println(hello);
+		
+		System.out.println(x+y);
+	}
+
+}
---
+++ src/com/grassjavastudy/ch01/SecondJavaClass.java
@@ -0,0 +1,15 @@
+package com.grassjavastudy.ch01;
+
+public class SecondJavaClass {
+
+	public static void main(String[] args) {
+		
+		int x = 35;
+		int y = 11;
+		
+		System.out.println(x-y);
+		System.out.println(x/y);
+
+	}
+
+}
